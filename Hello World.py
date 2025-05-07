b0322ea533f5fe73ced69d1f5bb1c728df9cc1e9
Leiranrongli <ranfeng_233@163.com>
2025-05-07 13:03:02 +0800

    字符串

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,10 @@
+# 欢迎来到python
 print("Hello world")
+
+## 字符串
+
+print('I love you')
+print("I love you") #不区分单双引号
+print() # 默认换行
+print("jam's home")
+print('"don\'t forget me"') #主要用来区分语句中的单\双引号,如语句中单双引号都存在，则应该使用转义字符
